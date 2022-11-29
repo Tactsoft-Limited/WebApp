671fc1199033954d38fe66e8214aa2ea75e44222
khpolash <polash250@gmail.com>
2022-11-29 16:25:57 +0600

    khp:addMeritalStatusController

---
+++ WebApp/Controllers/Configurations/MeritalStatusController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApp.Extensions;
+using WebApp.Helpers.Base;
+using WebApp.Service.Models.Configurations;
+using WebApp.Service.Services.Configurations;
+using WebApp.Services;
+using WebApp.Sql.Entities.Configurations;
+
+namespace WebApp.Controllers.Configurations
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeritalStatusController : GenericBaseController<MeritalStatus>
+    {
+        private readonly IMeritalStatusService  _meritalStatusService;
+
+        public MeritalStatusController(IMeritalStatusService meritalStatusService) : base(meritalStatusService)
+        {
+            _meritalStatusService = meritalStatusService;
+        }
+
+        [HttpGet("dropdown")]
+        public async Task<IActionResult> GetDropdownAsync(string searchText = null)
+        {
+            var res = await _meritalStatusService.GetDropdownAsync(searchText);
+
+            return new ApiOkActionResult(res);
+        }
+
+    }
+}
