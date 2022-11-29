c3476a0ab06cf2ce88a25bdb25098ac9f086e175
khpolash <polash250@gmail.com>
2022-11-29 16:32:21 +0600

    khp:add MeritalStatusCofiguration

---
+++ WebApp.Sql/EntityConfiguration/Configurations/MeritalStatusNameConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApp.Sql.Entities.Configurations;
+
+namespace WebApp.Sql.EntityConfiguration.Configurations
+{
+    public class MeritalStatusNameConfiguration : IEntityTypeConfiguration<MeritalStatus>
+    {
+        public void Configure(EntityTypeBuilder<MeritalStatus> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
