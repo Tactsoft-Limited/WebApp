47f53b8580345bbb35295730f83cb6c54aa4b91f
mdborhanuddinsobuj <borhanuddinmd46@gmail.com>
2022-11-30 10:55:03 +0600

    sobuj : supervisor migration

---
+++ WebApp.Sql/Migrations/20221130045006_Supervisor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Sql.Migrations
+{
+    public partial class Supervisor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Supervisor",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: true),
+                    SuppervisorSetupId = table.Column<long>(type: "bigint", nullable: true),
+                    EmployeeId = table.Column<long>(type: "bigint", nullable: true),
+                    SuppervisorEmployeeId = table.Column<long>(type: "bigint", nullable: true),
+                    IsDirectSupervisor = table.Column<bool>(type: "bit", nullable: false),
+                    EffectedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Remark = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserInformationId = table.Column<long>(type: "bigint", nullable: true),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedDateUtc = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedBy = table.Column<long>(type: "bigint", nullable: true),
+                    UpdatedDateUtc = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Supervisor", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Supervisor_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Supervisor_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Supervisor_UserInformations_UserInformationId",
+                        column: x => x.UserInformationId,
+                        principalTable: "UserInformations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Supervisor_EmployeeId",
+                table: "Supervisor",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Supervisor_UserId",
+                table: "Supervisor",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Supervisor_UserInformationId",
+                table: "Supervisor",
+                column: "UserInformationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Supervisor");
+        }
+    }
+}
