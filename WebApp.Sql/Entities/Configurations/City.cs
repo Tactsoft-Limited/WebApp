3420ae2cdf3be9e99c24d6f9a8b477d6720d3a9f
Nawaj <nawajshah68@gmail.com>
2022-12-01 11:32:16 +0600

    BranchinfolistAdd_nawaj

--- WebApp.Sql/Entities/Configurations/City.cs
+++ WebApp.Sql/Entities/Configurations/City.cs
@@ -11,5 +11,6 @@ namespace WebApp.Sql.Entities.Configurations
         public State State { get; set; }
         public IList<UserInformation> UserInformations { get; set; }
         public IList<Contact> Contacts { get; set; }
+        public IList<BranchInfo> BranchInfos { get; set; }
     }
 }
