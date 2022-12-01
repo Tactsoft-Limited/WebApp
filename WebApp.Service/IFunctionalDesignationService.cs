072cb562c1e411775ce3053aff128e8ca8525d9d
mdborhanuddinsobuj <borhanuddinmd46@gmail.com>
2022-12-01 14:51:16 +0600

    Sobuj: add IFunctionalDesignationService

---
+++ WebApp.Service/IFunctionalDesignationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Core;
+using WebApp.Service.Models.Enrols;
+using WebApp.Services;
+using WebApp.Sql.Entities.Enrols;
+
+namespace WebApp.Service
+{
+    public interface IFunctionalDesignationService:IBaseService<FunctionalDesignation>
+    {
+        //Task<FunctionalDesignationModel> GetSearchAsync(int pageIndex=CommonVariables.pageIndex,int pageSize=CommonVariables.pageSize,string searchText=null);
+        //Task<FunctionalDesignationModel> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null);
+        Task<FunctionalDesignationModel> GetFunctionalDesignationDetailAsync(long functionalDesignationId);
+        Task<FunctionalDesignationModel> AddFunctionalDesignationDetailAsync(FunctionalDesignationModel functionalDesignation);
+        Task<FunctionalDesignationModel> UpdateFunctionalDesignationDetailAsync(long functionalDesignationId, FunctionalDesignationModel model);
+    }
+}
