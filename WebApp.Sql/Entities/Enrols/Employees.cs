b522b8222863db43531472f3bf6257f0e0db2f38
khpolash <polash250@gmail.com>
2022-12-01 20:25:17 +0600

    khp: Add Language list

--- WebApp.Sql/Entities/Enrols/Employees.cs
+++ WebApp.Sql/Entities/Enrols/Employees.cs
@@ -41,5 +41,6 @@ namespace WebApp.Sql.Entities.Enrols
         public IList<Contact> Contacts { get; set; }   
         public IList<DepartmentSetup> DepartmentSetups { get; set;}
         public IList<Warning> Warnings { get; set; }
+        public IList<Language> Languages { get; set; }
     }
 }
