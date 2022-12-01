901f9d64db61c0e162cf40728d719e2f9596fcc3
khpolash <polash250@gmail.com>
2022-12-01 11:30:31 +0600

    khp: Add DepartmentSetup Service & Interface

---
+++ WebApp.Service/IDepartmentSetupService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Service.Models.Enrols;
+using WebApp.Services;
+using WebApp.Sql.Entities.Enrols;
+
+namespace WebApp.Service
+{
+    public interface IDepartmentSetupService:IBaseService<DepartmentSetup>
+    {
+        Task<DepartmentSetupModel> GetDepartmentSetupDetailAsync(long departmentSetupId);
+        Task<DepartmentSetupModel> AddDepartmentSetupDetailAsync(DepartmentSetupModel model);
+        Task<DepartmentSetupModel> UpdateDepartmentSetupDetailAsync(long departmentSetupId, DepartmentSetupModel model);
+        Task<DepartmentSetupModel> UpdateDepartmentSetupDetailAsync(long departmentSetupId, string model);
+    }
+}
