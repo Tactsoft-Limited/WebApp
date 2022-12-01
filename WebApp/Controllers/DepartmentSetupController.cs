d3bee5024b45a4015f796903bbc1084269c1c825
khpolash <polash250@gmail.com>
2022-12-01 11:28:35 +0600

    khp: Add DepartmentSetup Controller

---
+++ WebApp/Controllers/DepartmentSetupController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApp.Extensions;
+using WebApp.Helpers.Base;
+using WebApp.Service;
+using WebApp.Service.Models.Enrols;
+using WebApp.Services;
+using WebApp.Sql.Entities.Enrols;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentSetupController : GenericBaseController<DepartmentSetup>
+    {
+        private readonly IDepartmentSetupService _departmentSetupSservice;
+
+        public DepartmentSetupController(IDepartmentSetupService departmentSetupSservice) : base(departmentSetupSservice)
+        {
+            _departmentSetupSservice = departmentSetupSservice;
+        }
+
+        [HttpGet("{departmentSetupId}")]
+        public async Task<IActionResult> GetContactDetailsAsync(long departmentSetupId)
+        {
+            var res = await _departmentSetupSservice.GetDepartmentSetupDetailAsync(departmentSetupId);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpPost()]
+        public async Task<IActionResult> AddDepartmentSetupDetailAsync ([FromForm] DepartmentSetupModel model)
+        {
+            var res = await _departmentSetupSservice.AddDepartmentSetupDetailAsync(model);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpPut("{contactId}")]
+        public async Task<IActionResult> UpdateDepartmentSetupDetailAsync(long contactId, [FromForm] DepartmentSetupModel model)
+        {
+            var res = await _departmentSetupSservice.UpdateDepartmentSetupDetailAsync(contactId, model);
+            return new ApiOkActionResult(res);
+        }
+    }
+}
