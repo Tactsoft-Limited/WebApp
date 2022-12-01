73a38ced0d5a8df323e5bc7df83725a2f41a507d
mdborhanuddinsobuj <borhanuddinmd46@gmail.com>
2022-12-01 14:53:15 +0600

    sobuj: FunctionalDesignation Migration

--- WebApp.Sql/Migrations/20221201052018_FunctionalDesignationTable.Designer.cs
+++ WebApp.Sql/Migrations/20221201052018_FunctionalDesignationTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp.Sql;
 
 namespace WebApp.Sql.Migrations
 {
     [DbContext(typeof(WebAppContext))]
-    partial class WebAppContextModelSnapshot : ModelSnapshot
+    [Migration("20221201052018_FunctionalDesignationTable")]
+    partial class FunctionalDesignationTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
