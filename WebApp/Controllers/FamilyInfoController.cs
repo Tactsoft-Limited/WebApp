40e403ecbee7f952883ec9bed9a5efb4d1f3f859
khpolash <polash250@gmail.com>
2022-12-03 10:26:34 +0600

    khp: Add FimalyInfo Controller

---
+++ WebApp/Controllers/FamilyInfoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApp.Extensions;
+using WebApp.Helpers.Base;
+using WebApp.Service;
+using WebApp.Service.Models.Enrols;
+using WebApp.Services;
+using WebApp.Sql.Entities.Enrols;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FamilyInfoController : GenericBaseController<FamilyInfo>
+    {
+        private readonly IFamilyInfoService _familyInfoService;
+
+        public FamilyInfoController(IFamilyInfoService familyInfoService) : base(familyInfoService)
+        {
+            this._familyInfoService = familyInfoService;
+        }
+
+        [HttpGet("{familyInfoId}")]
+        public async Task<IActionResult> GetFamilyInfoDetailAsync(long familyInfoId)
+        {
+            var res = await _familyInfoService.GetFamilyInfoDetailAsync(familyInfoId);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpPost()]
+        public async Task<IActionResult> AddFamilyInfoDetailAsync([FromForm] FamilyInfoModel model)
+        {
+            var res = await _familyInfoService.AddFamilyInfoDetailAsync(model);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpPut("{familyInfoId}")]
+        public async Task<IActionResult> UpdateFamilyInfoDetailAsync(long familyInfoId, [FromForm] FamilyInfoModel employees)
+        {
+
+            var res = await _familyInfoService.UpdateFamilyInfoDetailAsync(familyInfoId, employees);
+
+            return new ApiOkActionResult(res);
+        }
+    }
+}
