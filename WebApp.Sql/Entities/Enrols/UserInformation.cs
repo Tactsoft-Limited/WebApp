1dc97f91898394eb2bd3c3e4c5c7bab4540e77af
khpolash <polash250@gmail.com>
2022-12-03 17:02:38 +0600

    khp:Update UserInformation Remove some Properties

--- WebApp.Sql/Entities/Enrols/UserInformation.cs
+++ WebApp.Sql/Entities/Enrols/UserInformation.cs
@@ -52,10 +52,7 @@ namespace WebApp.Sql.Entities.Enrols
         public IList<UserProfessionalInformation> UserProfessionalInformations { get; set; }
         public IList<UserCertification> UserCertifications { get; set; }
         public IList<UserSkill> UserSkills { get; set; }
-        public IList<Supervisor> Supervisors { get; set; }
-        public IList<FunctionalDesignation> FunctionalDesignations { get; set; }
-        public IList<EmployeeManagementCategory> EmployeeManagementCategories { get; set; }
-        public IList<BankInfo> BankInfos { get; set; }
+      
     }
 
     //public enum Gender
