8ce6abf16b4b113eb42568a2aee7d3598fbc9665
khpolash <polash250@gmail.com>
2022-12-04 16:54:16 +0600

    khp: Add Education Class GroupId to EducationGroupId and Nullable

--- WebApp.Sql/Entities/Enrols/Education.cs
+++ WebApp.Sql/Entities/Enrols/Education.cs
@@ -4,26 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApp.Sql.Entities.Configurations;
 using WebApp.Sql.Migrations;
 
 namespace WebApp.Sql.Entities.Enrols
 {
     public class Education:BaseEntity
     {
-        public long EmployeeId { get; set; }
-        public long GroupId { get; set; }
-        public long EducationTypeId { get; set; }
-        public long GradeId { get; set; }
+
+        public long? EmployeeId { get; set; }
+        public long? EgucationGroupId { get; set; }
+        public long? EducationTypeId { get; set; }
+        public long? GradeId { get; set; }
         public DateTime PassingYear { get; set; }
         public string Result { get; set; }
         public string Scale { get; set; }
-        public long InstituteId { get; set; }
+        public long? InstituteId { get; set; }
         public string ForeignDegree { get; set; }
         public string ProfessionalDegree { get; set; }
         public string LastEducation { get; set; }
         public string Remarks { get; set; }
 
         public Employees Employees { get; set; }
+        public EducationGroup EducationGroup { get; set; }
+        public EducationType EducationType { get; set; }
+        public Grade Grade { get; set; }
+        public Institute Institute { get; set; }
 
     }
 }
