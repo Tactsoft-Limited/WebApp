6169d736c63d9212fbde782bbb9c23263407855c
khpolash <polash250@gmail.com>
2022-12-04 15:27:21 +0600

    khp: Add DesignationSetup Repository & Interface in Sql

--- WebApp.Sql/Repositories/IDesignationSetupRepository.cs
+++ WebApp.Sql/Repositories/IDesignationSetupRepository.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApp.Core.Sqls;
+using WebApp.Sql.Entities.Enrols;
 
 namespace WebApp.Sql.Repositories
 {
