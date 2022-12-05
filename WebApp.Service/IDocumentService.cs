3787cf8ac5553805db7280aa2c17eac9024d9be9
mdborhanuddinsobuj <borhanuddinmd46@gmail.com>
2022-12-05 15:05:47 +0600

    sobuj : add IDocumentservice

---
+++ WebApp.Service/IDocumentService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Service.Models.Enrols;
+using WebApp.Services;
+using WebApp.Sql.Entities.Enrols;
+
+namespace WebApp.Service
+{
+    public interface IDocumentService:IBaseService<Document>
+    {
+        Task<DocumentModel> GetDocumentDetailAsync(long documentId);
+        Task<DocumentModel> AddDocumentDetailAsync(DocumentModel model);
+        Task<DocumentModel> UpdateDocumentDetailAsync(long documentId, DocumentModel model);
+        Task<DocumentModel> UpdateDocumentDetailAsync(long documentId, string model, List<IFormFile> file);
+    }
+}
