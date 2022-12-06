9a73f38524c91d89da7aee531925e688671c80d3
Nawaj <nawajshah68@gmail.com>
2022-12-06 11:36:40 +0600

    controlleradded_nawaj

---
+++ WebApp/Controllers/Configurations/AwardTypeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApp.Extensions;
+using WebApp.Helpers.Base;
+using WebApp.Service.Services.Configurations;
+using WebApp.Sql.Entities.Configurations;
+
+namespace WebApp.Controllers.Configurations
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AwardTypeController : GenericBaseController<AwardType>
+    {
+        private readonly IAwardTypeService _awardtypeService;
+
+        public AwardTypeController(IAwardTypeService awardtypeService) : base(awardtypeService)
+        {
+            _awardtypeService = awardtypeService;
+        }
+        [HttpGet("company_dropdown")]
+        public async Task<IActionResult> GetCompanyDropdownAsync(long? companyId = null, string searchText = null)
+        {
+            var res = await _awardtypeService.GetCompanyDropdownAsync(companyId, searchText);
+
+            return new ApiOkActionResult(res);
+        }
+    }
+}
