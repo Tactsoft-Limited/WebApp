a8469ad078b16f739c87befc62f708879a97e132
Nawaj <nawajshah68@gmail.com>
2022-12-06 11:31:17 +0600

    serviceadded_nawaj

---
+++ WebApp.Service/Services/Configurations/IOpeningYearService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Core;
+using WebApp.Core.Collections;
+using WebApp.Service.Models.Configurations;
+using WebApp.Services;
+using WebApp.Sql.Entities.Configurations;
+
+namespace WebApp.Service.Services.Configurations
+{
+    public interface IOpeningYearService : IBaseService<OpeningYear>
+    {
+        Task<Dropdown<OpeningYearModel>> GetCompanyDropdownAsync(long? companyId = null, string searchText = null, int size = CommonVariables.DropdownSize);
+    }
+}
