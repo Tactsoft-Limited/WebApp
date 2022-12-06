4959ac13fcee956708c743c3846018d9cc6a303e
Nawaj <nawajshah68@gmail.com>
2022-12-06 11:25:13 +0600

    modeladded_nawaj

---
+++ WebApp.Service/Models/Configurations/LeaveTypeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Service.Models.Configurations
+{
+    public class LeaveTypeModel : MasterModel
+    {
+        public long? CompanyId { get; set; }
+        public string LeaveTypeName { get; set; }
+        public float FullPayment { get; set; }
+        public float HalfPayment { get; set; }
+        public bool IsMeternal { get; set; }
+        public bool IsUnpaid { get; set; }
+        public bool IsPartialLeave { get; set; }
+    }
+}
