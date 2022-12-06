d7ccd2e8b5edaac276dc04a6ee0f58b39331f361
Nawaj <nawajshah68@gmail.com>
2022-12-06 11:32:47 +0600

    serviceadded_nawaj

---
+++ WebApp.Service/Services/Configurations/WarningTypeService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Core;
+using WebApp.Core.Collections;
+using WebApp.Service.Models.Configurations;
+using WebApp.Services;
+using WebApp.Sql.Entities.Configurations;
+
+namespace WebApp.Service.Services.Configurations
+{
+    public class WarningTypeService : BaseService<WarningType>,IWarningTypeService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public WarningTypeService(IUnitOfWork unitOfWork,
+                IMapper mapper) : base(unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<Dropdown<WarningTypeModel>> GetCompanyDropdownAsync(long? companyId = null,
+    string searchText = null,
+    int size = CommonVariables.DropdownSize)
+        {
+            var data = await _unitOfWork.Repository<WarningType>().GetDropdownAsync(
+                s => ((string.IsNullOrEmpty(searchText) || s.Company.CompanyName.Contains(searchText))
+                    && companyId == null || s.CompanyId == companyId),
+                o => o.OrderBy(ob => ob.Id),
+                se => new WarningTypeModel { Id = se.Id, WarningTypeName = se.WarningTypeName, CompanyId = se.CompanyId },
+                size);
+
+            return data;
+        }
+    }
+}
