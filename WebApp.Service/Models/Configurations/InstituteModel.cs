1e6b69bed256c9702f8a49c0b366b50ff6f6079a
Nawaj <nawajshah68@gmail.com>
2022-12-06 11:24:58 +0600

    modeladded_nawaj

---
+++ WebApp.Service/Models/Configurations/InstituteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Service.Models.Configurations
+{
+    public class InstituteModel : MasterModel
+    {
+        public long? CompanyId { get; set; }
+        public string InstituteName { get; set; }
+        public string Address { get; set; }
+    }
+}
