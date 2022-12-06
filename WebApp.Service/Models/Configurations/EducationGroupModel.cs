6076c91f3e42d2225e31b804566612f549751caa
Nawaj <nawajshah68@gmail.com>
2022-12-06 11:23:55 +0600

    modeladded_nawaj

---
+++ WebApp.Service/Models/Configurations/EducationGroupModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Service.Models.Configurations
+{
+    public class EducationGroupModel : MasterModel
+    {
+        public long? CompanyId { get; set; }
+        public string EducationGroupName { get; set; }
+        public bool Status { get; set; }
+        
+    }
+}
