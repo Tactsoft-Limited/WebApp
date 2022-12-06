b9698d2964cef875b351db90dfa3847b9fe0c6b0
Nawaj <nawajshah68@gmail.com>
2022-12-06 11:37:01 +0600

    controlleradded_nawaj

---
+++ WebApp/Controllers/Configurations/DocumentCategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApp.Extensions;
+using WebApp.Helpers.Base;
+using WebApp.Service.Services.Configurations;
+using WebApp.Sql.Entities.Configurations;
+
+namespace WebApp.Controllers.Configurations
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentCategoryController : GenericBaseController<DocumentCategorie>
+    {
+        private readonly IDocumentCategoryService _documentcategoryService;
+
+        public DocumentCategoryController(IDocumentCategoryService documentcategoryService) : base(documentcategoryService)
+        {
+            _documentcategoryService = documentcategoryService;
+        }
+        [HttpGet("company_dropdown")]
+        public async Task<IActionResult> GetCompanyDropdownAsync(long? companyId = null, string searchText = null)
+        {
+            var res = await _documentcategoryService.GetCompanyDropdownAsync(companyId, searchText);
+
+            return new ApiOkActionResult(res);
+        }
+    }
+}
