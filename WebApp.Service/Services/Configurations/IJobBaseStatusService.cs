3c56c7dc88f6e1fbc4c221d4422a8fcce088523e
Nawaj <nawajshah68@gmail.com>
2022-12-06 11:30:32 +0600

    serviceadded_nawaj

---
+++ WebApp.Service/Services/Configurations/IJobBaseStatusService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Core;
+using WebApp.Core.Collections;
+using WebApp.Service.Models.Configurations;
+using WebApp.Services;
+using WebApp.Sql.Entities.Configurations;
+
+namespace WebApp.Service.Services.Configurations
+{
+    public interface IJobBaseStatusService : IBaseService<JobBaseStatus>
+    {
+        Task<Dropdown<JobBaseStatusModel>> GetCompanyDropdownAsync(long? companyId = null, string searchText = null, int size = CommonVariables.DropdownSize);
+        Task<Dropdown<JobBaseStatusModel>> GetProjectDropdownAsync(long? companyId = null, string searchText = null, int size = CommonVariables.DropdownSize);
+        Task<Dropdown<JobBaseStatusModel>> GetBranchDropdownAsync(long? companyId = null, string searchText = null, int size = CommonVariables.DropdownSize);
+    }
+}
