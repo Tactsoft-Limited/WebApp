ea2e9fb94828c17c1e6a9588aac57cd9686a3fb8
Nawaj <nawajshah68@gmail.com>
2022-12-06 11:28:25 +0600

    serviceadded_nawaj

---
+++ WebApp.Service/Services/Configurations/EmploymentCategoryService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Core;
+using WebApp.Core.Collections;
+using WebApp.Service.Models.Configurations;
+using WebApp.Services;
+using WebApp.Sql.Entities.Configurations;
+
+namespace WebApp.Service.Services.Configurations
+{
+   public class EmploymentCategoryService : BaseService<EmploymentCategorie> , IEmploymentCategoryService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public EmploymentCategoryService(IUnitOfWork unitOfWork,
+                IMapper mapper) : base(unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<Dropdown<EmploymentCategoryModel>> GetCompanyDropdownAsync(long? companyId = null,
+    string searchText = null,
+    int size = CommonVariables.DropdownSize)
+        {
+            var data = await _unitOfWork.Repository<EmploymentCategorie>().GetDropdownAsync(
+                s => ((string.IsNullOrEmpty(searchText) || s.Company.CompanyName.Contains(searchText))
+                    && companyId == null || s.CompanyId == companyId),
+                o => o.OrderBy(ob => ob.Id),
+                se => new EmploymentCategoryModel { Id = se.Id, CategoryName = se.CategoryName, CompanyId = se.CompanyId },
+                size);
+
+            return data;
+        }
+    }
+}
