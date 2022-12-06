23193b5665182069ceaa0c76abb4a8872a704ef1
Nawaj <nawajshah68@gmail.com>
2022-12-06 11:38:48 +0600

    controlleradded_nawaj

---
+++ WebApp/Controllers/Configurations/NewApprovalWorkflowController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApp.Extensions;
+using WebApp.Helpers.Base;
+using WebApp.Service.Services.Configurations;
+using WebApp.Sql.Entities.Configurations;
+
+namespace WebApp.Controllers.Configurations
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewApprovalWorkflowController : GenericBaseController<NewApprovalWorkflow>
+    {
+        private readonly INewApprovalWorkflowService _newapprovalworkflowService;
+
+        public NewApprovalWorkflowController(INewApprovalWorkflowService newapprovalworkflowService) : base(newapprovalworkflowService)
+        {
+            _newapprovalworkflowService = newapprovalworkflowService;
+        }
+        [HttpGet("company_dropdown")]
+        public async Task<IActionResult> GetCompanyDropdownAsync(long? companyId = null, string searchText = null)
+        {
+            var res = await _newapprovalworkflowService.GetCompanyDropdownAsync(companyId, searchText);
+
+            return new ApiOkActionResult(res);
+        }
+    }
+}
