2a198865f6da50aa72f3de12e1ed6a9ed618881a
khpolash <polash250@gmail.com>
2022-12-13 22:38:59 +0600

    khp: Add Search and Filter Option

--- WebApp.Service/IEducationService.cs
+++ WebApp.Service/IEducationService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApp.Core.Collections;
+using WebApp.Core;
 using WebApp.Service.Models.Enrols;
 using WebApp.Services;
 using WebApp.Sql.Entities.Enrols;
@@ -11,6 +13,8 @@ namespace WebApp.Service
 {
     public interface IEducationService:IBaseService<Education>
     {
+        Task<Paging<EducationModel>> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null);
+        Task<Paging<EducationModel>> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string filterText1 = null);
         Task<EducationModel> GetEducationDetailsAsync(long educationId);
         Task<EducationModel> AddEducationDetailsAsync(EducationModel model);
         Task<EducationModel> UpdateEducationDetailsAsync(long educationId, EducationModel model);
