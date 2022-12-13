bc05bc2061fea5340fcd16633d4dd06ef4a15ec4
khpolash <polash250@gmail.com>
2022-12-13 22:05:45 +0600

    khp: Add Search and Filter

--- WebApp.Service/IContactService.cs
+++ WebApp.Service/IContactService.cs
@@ -14,6 +14,8 @@ namespace WebApp.Service
 {
     public interface IContactService:IBaseService<Contact>
     {
+        Task<Paging<ContactModel>> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null);
+        Task<Paging<ContactModel>> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string filterText1 = null);
         Task<ContactModel> GetContactDetailsAsync(long contactId);
         Task<ContactModel> AddContactDetailsAsync(ContactModel model);
         Task<ContactModel> UpdateContactDetailsAsync(long contactId, ContactModel model);
