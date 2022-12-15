3f51930d06cc8696acde64a9e0c36a0d270e8a11
khpolash <polash250@gmail.com>
2022-12-15 10:39:23 +0600

    khp: add migration for rename column name

--- WebApp.Sql/Migrations/20221214065048_Rename StateId properties on BranchInfo.Designer.cs
+++ WebApp.Sql/Migrations/20221214065048_Rename StateId properties on BranchInfo.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp.Sql;
 
 namespace WebApp.Sql.Migrations
 {
     [DbContext(typeof(WebAppContext))]
-    partial class WebAppContextModelSnapshot : ModelSnapshot
+    [Migration("20221214065048_Rename StateId properties on BranchInfo")]
+    partial class RenameStateIdpropertiesonBranchInfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
