7a86d382247ef0056086206726e05a94e5ad59a9
mdsobujislam <sobujislam674@gmail.com>
2022-12-18 12:05:05 +0600

    sobuj : add search n filter

--- WebApp.Service/FunctionalDesignationService.cs
+++ WebApp.Service/FunctionalDesignationService.cs
@@ -34,6 +34,17 @@ namespace WebApp.Service
             return new FunctionalDesignationModel();
         }
 
+        public async Task<Paging<FunctionalDesignationModel>> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string filterText1 = null)
+        {
+            var data = await _unitOfWork.Repository<FunctionalDesignation>().GetPageAsync(pageIndex, pageSize,
+                s=>(string.IsNullOrEmpty(filterText1) || s.Employees.Name.Contains(filterText1)),
+                o=>o.OrderBy(ob=>ob.Id),
+                se=>se,
+                i =>i.User
+                );
+            var res = data.ToPagingModel<FunctionalDesignation, FunctionalDesignationModel>(_mapper);
+            return res;
+        }
 
         public async Task<FunctionalDesignationModel> GetFunctionalDesignationDetailAsync(long functionalDesignationId)
         {
@@ -42,6 +53,17 @@ namespace WebApp.Service
             return response;
         }
 
+        public async Task<Paging<FunctionalDesignationModel>> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null)
+        {
+            var data = await _unitOfWork.Repository<FunctionalDesignation>().GetPageAsync(pageIndex, pageSize,
+                s =>(string.IsNullOrEmpty(searchText) || s.Employees.Name.Contains(searchText)),
+                o=>o.OrderBy(ob=>ob.Id),
+                se=>se,
+                i=>i.User
+                );
+            var res = data.ToPagingModel<FunctionalDesignation, FunctionalDesignationModel>(_mapper);
+            return res;
+        }
 
         public async Task<FunctionalDesignationModel> UpdateFunctionalDesignationDetailAsync(long functionalDesignationId, FunctionalDesignationModel model)
         {
