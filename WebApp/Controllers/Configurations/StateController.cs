b7aa8edb0b99c65024b872e1e21ed241bee421cd
khpolash <polash250@gmail.com>
2022-12-19 17:14:05 +0600

    khp: Implement Crud Methods

--- WebApp/Controllers/Configurations/StateController.cs
+++ WebApp/Controllers/Configurations/StateController.cs
@@ -26,5 +26,35 @@ namespace WebApp.Controllers.Configurations
 
             return new ApiOkActionResult(res);
         }
+        [HttpGet("search")]
+        public async Task<IActionResult> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null)
+        {
+            var res = await _stateService.GetSearchAsync(pageIndex, pageSize, searchText);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpGet("{stateId}")]
+        public async Task<IActionResult> GetContactDetailsAsync(long stateId)
+        {
+            var res = await _stateService.GetCountryDetailsAsync(stateId);
+
+            return new ApiOkActionResult(res);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> AddBloodGroupDetailsAsync([FromForm] StateModel model)
+        {
+            var res = await _stateService.AddCountryDetailsAsync(model);
+
+            return new ApiOkActionResult(res);
+        }
+
+        [HttpPut("{stateId}")]
+        public async Task<IActionResult> UpdateContactDetailsAsync(long stateId, [FromForm] StateModel model)
+        {
+            var res = await _stateService.UpdateCountryDetailsAsync(stateId, model);
+
+            return new ApiOkActionResult(res);
+        }
     }
 }
