74bd0bf44e27612ada973a38738f8a47f3be27fc
khpolash <polash250@gmail.com>
2022-12-20 12:45:31 +0600

    khp: merital status  curd added

--- WebApp.Service/Services/Configurations/IMeritalStatusService.cs
+++ WebApp.Service/Services/Configurations/IMeritalStatusService.cs
@@ -14,5 +14,11 @@ namespace WebApp.Service.Services.Configurations
     public interface IMeritalStatusService:IBaseService<MeritalStatus>
     {
         Task<Dropdown<MeritalStatusModel>> GetDropdownAsync(string searchText = null, int size = CommonVariables.DropdownSize);
+        Task<Paging<MeritalStatusModel>> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null);
+        Task<Paging<MeritalStatusModel>> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string filterText1 = null);
+        Task<MeritalStatusModel> AddMeritalStatusDetailsAsync(MeritalStatusModel model);
+        Task<MeritalStatusModel> UpdateMeritalStatusDetailsAsync(long meritalStatusId, MeritalStatusModel model);
+        Task<MeritalStatusModel> UpdateMeritalStatusDetailsAsync(long meritalStatusId, string model);
+        Task<MeritalStatusModel> GetMeritalStatusDetailsAsync(long meritalStatusId);
     }
 }
--- WebApp.Service/Services/Configurations/MeritalStatusService.cs
+++ WebApp.Service/Services/Configurations/MeritalStatusService.cs
@@ -23,16 +23,57 @@ namespace WebApp.Service.Services.Configurations
             _mapper = mapper;
         }
 
+        public async Task<MeritalStatusModel> AddMeritalStatusDetailsAsync(MeritalStatusModel model)
+        {
+            var entity = _mapper.Map<MeritalStatusModel, MeritalStatus>(model);
+            await _unitOfWork.Repository<MeritalStatus>().UpdateAsync(entity);
+            await _unitOfWork.CompleteAsync();
+            return new MeritalStatusModel();
+        }
+
+        public async Task<MeritalStatusModel> GetMeritalStatusDetailsAsync(long meritalStatusId)
+        {
+            var data = await _unitOfWork.Repository<MeritalStatus>().FirstOrDefaultAsync(f=>f.Id== meritalStatusId);
+            return  _mapper.Map<MeritalStatus, MeritalStatusModel>(data);
+        }
+
         public async Task<Dropdown<MeritalStatusModel>> GetDropdownAsync(string searchText = null, int size = CommonVariables.DropdownSize)
         {
             var data = await _unitOfWork.Repository<MeritalStatus>().GetDropdownAsync(
                 p=>(string.IsNullOrEmpty(searchText)|| p.MeritalStatusName.Contains(searchText)),
                 o=>o.OrderBy(ob=>ob.Id),
                 se=> new MeritalStatusModel { Id = se.Id, MeritalStatusName = se.MeritalStatusName},
-                size
-                );
+                size);
 
             return data;
+        }
+
+        public Task<Paging<MeritalStatusModel>> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string filterText1 = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Paging<MeritalStatusModel>> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null)
+        {
+            var data = await _unitOfWork.Repository<MeritalStatus>().GetPageAsync(pageIndex, pageSize,
+                p=>(string.IsNullOrEmpty(searchText)|p.MeritalStatusName.Contains(searchText)),
+                o=>o.OrderBy(ob=>ob.Id));
+            return data.ToPagingModel<MeritalStatus, MeritalStatusModel>(_mapper);
+        }
+
+        public async Task<MeritalStatusModel> UpdateMeritalStatusDetailsAsync(long meritalStatusId, MeritalStatusModel model)
+        {
+            var entity = _mapper.Map<MeritalStatusModel, MeritalStatus>(model);
+
+            await _unitOfWork.Repository<MeritalStatus>().UpdateAsync(entity);
+            await _unitOfWork.CompleteAsync();
+
+            return new MeritalStatusModel();
+        }
+
+        public Task<MeritalStatusModel> UpdateMeritalStatusDetailsAsync(long meritalStatusId, string model)
+        {
+            throw new NotImplementedException();
         }
     }
 }
