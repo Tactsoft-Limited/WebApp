295c65a5698418f2282ceb01cb20ba292fcc7905
Nawaj <nawajshah68@gmail.com>
2022-12-24 11:34:40 +0600

    added_search-post_put-nawaj

--- WebApp/Controllers/Configurations/AwardTypeController.cs
+++ WebApp/Controllers/Configurations/AwardTypeController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using WebApp.Core;
 using WebApp.Extensions;
 using WebApp.Helpers.Base;
+using WebApp.Service.Models.Configurations;
 using WebApp.Service.Services.Configurations;
 using WebApp.Sql.Entities.Configurations;
 
@@ -18,10 +20,46 @@ namespace WebApp.Controllers.Configurations
         {
             _awardtypeService = awardtypeService;
         }
-        [HttpGet("company_dropdown")]
-        public async Task<IActionResult> GetCompanyDropdownAsync(long? companyId = null, string searchText = null)
+        [HttpGet("dropdown")]
+        public async Task<IActionResult> GetDropdownAsync( string searchText = null)
         {
-            var res = await _awardtypeService.GetCompanyDropdownAsync(companyId, searchText);
+            var res = await _awardtypeService.GetDropdownAsync( searchText);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpGet("search")]
+        public async Task<IActionResult> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null)
+        {
+            var res = await _awardtypeService.GetSearchAsync(pageIndex, pageSize, searchText);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpGet("filter")]
+        public async Task<IActionResult> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string filterText1 = null /*string filterText2 = null*/)
+        {
+            var res = await _awardtypeService.GetFilterAsync(pageIndex, pageSize, filterText1 /*filterText2*/);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpGet("{awardtypeId}")]
+        public async Task<IActionResult> GetAwardTypeDetailAsync(long awardtypeId)
+        {
+            var res = await _awardtypeService.GetAwardTypeDetailAsync(awardtypeId);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpPost()]
+        public async Task<IActionResult> AddAwardTypeDetailAsync([FromForm] AwardTypeModel model)
+        {
+            var res = await _awardtypeService.AddAwardTypeDetailAsync(model);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpPut("{awardtypeId}")]
+        public async Task<IActionResult> UpdateAwardTypeDetailAsync(long awardtypeId, [FromForm] AwardTypeModel awardtype)
+        {
+
+            var res = await _awardtypeService.UpdateAwardTypeDetailAsync(awardtypeId, awardtype);
 
             return new ApiOkActionResult(res);
         }
