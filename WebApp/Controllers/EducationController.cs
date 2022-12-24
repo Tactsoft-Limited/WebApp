7b697f3b90e565d82045f595d4e76a65ee5cb07f
khpolash <polash250@gmail.com>
2022-12-24 11:21:47 +0600

    khp: add search & filter option

--- WebApp/Controllers/EducationController.cs
+++ WebApp/Controllers/EducationController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using WebApp.Core;
 using WebApp.Extensions;
 using WebApp.Helpers.Base;
 using WebApp.Service;
@@ -19,6 +20,21 @@ namespace WebApp.Controllers
         public EducationController(IEducationService educationService) : base(educationService)
         {
             this._educationService = educationService;
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null)
+        {
+            var res = await _educationService.GetSearchAsync(pageIndex, pageSize, searchText);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpGet("filter")]
+        public async Task<IActionResult> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string filterText1 = null /*string filterText2 = null*/)
+        {
+            var res = await _educationService.GetFilterAsync(pageIndex, pageSize, filterText1 /*filterText2*/);
+
+            return new ApiOkActionResult(res);
         }
 
         [HttpGet("{educationId}")]
