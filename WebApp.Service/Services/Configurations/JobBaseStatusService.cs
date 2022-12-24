257c0fc3b5257b8ae5257618af50f4a56e013656
Nawaj <nawajshah68@gmail.com>
2022-12-24 11:30:54 +0600

    added_search-post_put-nawaj

--- WebApp.Service/Services/Configurations/JobBaseStatusService.cs
+++ WebApp.Service/Services/Configurations/JobBaseStatusService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ using WebApp.Core.Collections;
 using WebApp.Service.Models.Configurations;
 using WebApp.Services;
 using WebApp.Sql.Entities.Configurations;
+using WebApp.Sql.Entities.Enrols;
 
 namespace WebApp.Service.Services.Configurations
 {
@@ -23,47 +25,72 @@ namespace WebApp.Service.Services.Configurations
             this._unitOfWork = unitOfWork;
             _mapper = mapper;
         }
-        public async Task<Dropdown<JobBaseStatusModel>> GetCompanyDropdownAsync(long? companyId = null,
-string searchText = null,
-int size = CommonVariables.DropdownSize)
+        public async Task<Paging<JobBaseStatusModel>> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null)
         {
-            var data = await _unitOfWork.Repository<JobBaseStatus>().GetDropdownAsync(
-                s => ((string.IsNullOrEmpty(searchText) || s.Company.CompanyName.Contains(searchText))
-                    && companyId == null || s.CompanyId == companyId),
+            var data = await _unitOfWork.Repository<JobBaseStatus>().GetPageAsync(pageIndex,
+                pageSize,
+                s => (string.IsNullOrEmpty(searchText) || s.JobBaseStatusTitle.Contains(searchText)),
                 o => o.OrderBy(ob => ob.Id),
-                se => new JobBaseStatusModel { Id = se.Id, JobBaseStatusTitle = se.JobBaseStatusTitle, CompanyId = se.CompanyId },
+                se => se);
 
-            size);
+            var response = data.ToPagingModel<JobBaseStatus, JobBaseStatusModel>(_mapper);
 
-            return data;
+            return response;
         }
-        public async Task<Dropdown<JobBaseStatusModel>> GetBranchDropdownAsync(long? branchId = null,
-    string searchText = null,
-    int size = CommonVariables.DropdownSize)
+        public async Task<Paging<JobBaseStatusModel>> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string filterText1 = null)
         {
-            var data = await _unitOfWork.Repository<JobBaseStatus>().GetDropdownAsync(
-                s => ((string.IsNullOrEmpty(searchText) || s.Branch.BranchName.Contains(searchText))
-                    && branchId == null || s.BranchId == branchId),
+            var data = await _unitOfWork.Repository<JobBaseStatus>().GetPageAsync(pageIndex,
+                pageSize,
+                s => ((string.IsNullOrEmpty(filterText1) || s.JobBaseStatusTitle.Contains(filterText1))),
+                //|| (string.IsNullOrEmpty(filterText2) || s.Lastname.Contains(filterText2))),
                 o => o.OrderBy(ob => ob.Id),
-                se => new JobBaseStatusModel { Id = se.Id, JobBaseStatusTitle = se.JobBaseStatusTitle, BranchId = se.BranchId },
+                se => se);
 
-            size);
+            var response = data.ToPagingModel<JobBaseStatus, JobBaseStatusModel>(_mapper);
 
-            return data;
+            return response;
         }
-        public async Task<Dropdown<JobBaseStatusModel>> GetProjectDropdownAsync(long? projectId = null,
-    string searchText = null,
-    int size = CommonVariables.DropdownSize)
+        public async Task<JobBaseStatusModel> GetJobBaseStatusDetailAsync(long jobbasestatusId)
         {
-            var data = await _unitOfWork.Repository<JobBaseStatus>().GetDropdownAsync(
-                s => ((string.IsNullOrEmpty(searchText) || s.Project.ProjectName.Contains(searchText))
-                    && projectId == null || s.ProjectId == projectId),
-                o => o.OrderBy(ob => ob.Id),
-                se => new JobBaseStatusModel { Id = se.Id, JobBaseStatusTitle = se.JobBaseStatusTitle, ProjectId = se.ProjectId },
+            var data = await _unitOfWork.Repository<JobBaseStatus>().FirstOrDefaultAsync(f => f.Id == jobbasestatusId,
+                o => o.OrderBy(ob => ob.Id)
+                );
 
-            size);
 
-            return data;
+            var response = _mapper.Map<JobBaseStatus, JobBaseStatusModel>(data);
+
+            return response;
         }
+        public async Task<JobBaseStatusModel> AddJobBaseStatusDetailAsync(JobBaseStatusModel jobbasestatus)
+        {
+
+
+            var entity = _mapper.Map<JobBaseStatusModel, JobBaseStatus>(jobbasestatus);
+
+            await _unitOfWork.Repository<JobBaseStatus>().UpdateAsync(entity);
+            await _unitOfWork.CompleteAsync();
+
+            return new JobBaseStatusModel();
+        }
+        public async Task<JobBaseStatusModel> UpdateJobBaseStatusDetailAsync(long jobbasestatusId, JobBaseStatusModel jobbasestatus)
+        {
+            var entity = _mapper.Map<JobBaseStatusModel, JobBaseStatus>(jobbasestatus);
+
+            await _unitOfWork.Repository<JobBaseStatus>().UpdateAsync(entity);
+            await _unitOfWork.CompleteAsync();
+
+            return new JobBaseStatusModel();
+        }
+        public async Task<JobBaseStatusModel> UpdateJobBaseStatusDetailAsync(long jobbasestatusId, string model)
+        {
+            var jobbasestatus = JsonConvert.DeserializeObject<JobBaseStatusModel>(model);
+            var entity = _mapper.Map<JobBaseStatusModel, JobBaseStatus>(jobbasestatus);
+
+            await _unitOfWork.Repository<JobBaseStatus>().UpdateAsync(entity);
+            await _unitOfWork.CompleteAsync();
+
+            return new JobBaseStatusModel();
+        }
+
     }
 }
