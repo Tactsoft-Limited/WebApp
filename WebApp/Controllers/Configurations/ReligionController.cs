96d90da7dd6549daf33011a24772adbebce5b020
khpolash <polash250@gmail.com>
2022-12-24 11:17:09 +0600

    khp: Add religion controller

--- WebApp/Controllers/Configurations/ReligionController.cs
+++ WebApp/Controllers/Configurations/ReligionController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using WebApp.Core;
 using WebApp.Extensions;
 using WebApp.Helpers.Base;
+using WebApp.Service.Models.Configurations;
 using WebApp.Service.Services.Configurations;
 using WebApp.Services;
 using WebApp.Sql.Entities.Configurations;
@@ -28,6 +30,36 @@ namespace WebApp.Controllers.Configurations
             return new ApiOkActionResult(res);
         }
 
+        [HttpGet("search")]
+        public async Task<IActionResult> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null)
+        {
+            var res = await _religionService.GetSearchAsync(pageIndex, pageSize, searchText);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpGet("{religionId}")]
+        public async Task<IActionResult> GetReligionDetailsAsync(long religionId)
+        {
+            var res = await _religionService.GetReligionDetailsAsync(religionId);
+
+            return new ApiOkActionResult(res);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> AddReligionDetailsAsync([FromForm] ReligionModel model)
+        {
+            var res = await _religionService.AddReligionDetailsAsync(model);
+
+            return new ApiOkActionResult(res);
+        }
+
+        [HttpPut("{religionId}")]
+        public async Task<IActionResult> UpdateReligionDetailsAsync(long religionId, [FromForm] ReligionModel model)
+        {
+            var res = await _religionService.UpdateReligionDetailsAsync(religionId, model);
+
+            return new ApiOkActionResult(res);
+        }
 
     }
 }
