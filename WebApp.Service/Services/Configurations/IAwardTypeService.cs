aae77745e4750b63e1464ae91debe125eec748fd
Nawaj <nawajshah68@gmail.com>
2022-12-24 11:28:45 +0600

    added_search-post_put-nawaj

--- WebApp.Service/Services/Configurations/IAwardTypeService.cs
+++ WebApp.Service/Services/Configurations/IAwardTypeService.cs
@@ -13,6 +13,12 @@ namespace WebApp.Service.Services.Configurations
 {
     public interface IAwardTypeService : IBaseService<AwardType>
     {
-        Task<Dropdown<AwardTypeModel>> GetCompanyDropdownAsync(long? companyId = null, string searchText = null, int size = CommonVariables.DropdownSize);
+        Task<Dropdown<AwardTypeModel>> GetDropdownAsync(string searchText = null, int size = CommonVariables.DropdownSize);
+        Task<Paging<AwardTypeModel>> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null);
+        Task<Paging<AwardTypeModel>> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string filterText1 = null);
+        Task<AwardTypeModel> GetAwardTypeDetailAsync(long awardtypeId);
+        Task<AwardTypeModel> AddAwardTypeDetailAsync(AwardTypeModel model);
+        Task<AwardTypeModel> UpdateAwardTypeDetailAsync(long awardtypeId, AwardTypeModel model);
+        Task<AwardTypeModel> UpdateAwardTypeDetailAsync(long awardtypeId, string model);
     }
 }
