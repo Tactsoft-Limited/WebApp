2d7eae341f7b4cf5714590b7bf244d1666a341d1
khpolash <polash250@gmail.com>
2022-12-24 11:16:51 +0600

    khp: Add religion service

--- WebApp.Service/Services/Configurations/IReligionService.cs
+++ WebApp.Service/Services/Configurations/IReligionService.cs
@@ -10,5 +10,12 @@ namespace WebApp.Service.Services.Configurations
     public interface IReligionService:IBaseService<Religion>
     {
         Task<Dropdown<ReligionModel>> GetDropdownAsync(string searchText = null, int size = CommonVariables.DropdownSize);
+        Task<Paging<ReligionModel>> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null);
+        Task<Paging<ReligionModel>> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string filterText1 = null);
+
+        Task<ReligionModel> AddReligionDetailsAsync(ReligionModel model);
+        Task<ReligionModel> UpdateReligionDetailsAsync(long religionId, ReligionModel model);
+        Task<ReligionModel> UpdateReligionDetailsAsync(long religionId, string model);
+        Task<ReligionModel> GetReligionDetailsAsync(long religionId);
     }
 }
