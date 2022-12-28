ebe4ed684f40607ccbff62b67adea7b5e74c87d6
mdsobujislam <sobujislam674@gmail.com>
2022-12-28 16:08:25 +0600

    sobuj: add services

--- WebApp.Service/Services/Configurations/HobbyTypeService.cs
+++ WebApp.Service/Services/Configurations/HobbyTypeService.cs
@@ -7,8 +7,10 @@ using System.Threading.Tasks;
 using WebApp.Core;
 using WebApp.Core.Collections;
 using WebApp.Service.Models.Configurations;
+using WebApp.Service.Models.Enrols;
 using WebApp.Services;
 using WebApp.Sql.Entities.Configurations;
+using WebApp.Sql.Entities.Enrols;
 
 namespace WebApp.Service.Services.Configurations
 {
@@ -23,6 +25,15 @@ namespace WebApp.Service.Services.Configurations
             this._mapper = mapper;
         }
 
+        public async Task<HobbyTypeModel> AddHobbyTypeDetailsAsync(HobbyTypeModel model)
+        {
+            var entity = _mapper.Map<HobbyTypeModel, HobbyType>(model);
+            await _unitOfWork.Repository<HobbyType>().UpdateAsync(entity);
+            await _unitOfWork.CompleteAsync();
+
+            return new HobbyTypeModel();
+        }
+
         public async Task<Dropdown<HobbyTypeModel>> GetDropdownAsync(string searchText = null, int size = CommonVariables.DropdownSize)
         {
             var data = await _unitOfWork.Repository<HobbyType>().GetDropdownAsync(
@@ -32,5 +43,44 @@ namespace WebApp.Service.Services.Configurations
                 size);
             return data;
         }
+
+        public async Task<Paging<HobbyTypeModel>> GetFilterAsync(int pageIndex = 0, int pageSize = 10, string filterText1 = null)
+        {
+            var data = await _unitOfWork.Repository<HobbyType>().GetPageAsync(pageIndex, pageSize,
+                s => (string.IsNullOrEmpty(filterText1) || s.HobbyTypeName.Contains(filterText1)),
+                o => o.OrderBy(ob => ob.Id),
+                se => se);
+            var response = data.ToPagingModel<HobbyType, HobbyTypeModel>(_mapper);
+            return response;
+        }
+
+        public async Task<HobbyTypeModel> GetHobbyTypeDetailsAsync(long hobbyTypeId)
+        {
+            var data = await _unitOfWork.Repository<HobbyType>().FirstOrDefaultAsync(f => f.Id == hobbyTypeId, o => o.OrderBy(ob => ob.Id)
+                );
+            var response = _mapper.Map<HobbyType, HobbyTypeModel>(data);
+            return response;
+        }
+
+        public async Task<Paging<HobbyTypeModel>> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null)
+        {
+            var data = await _unitOfWork.Repository<HobbyType>().GetPageAsync(pageIndex, pageSize,
+                s => ((string.IsNullOrEmpty(searchText) || s.HobbyTypeName.Contains(searchText)))
+                || (string.IsNullOrEmpty(searchText) || s.HobbyTypeName.Contains(searchText)),
+                o => o.OrderBy(ob => ob.Id),
+                se => se);
+            var response = data.ToPagingModel<HobbyType, HobbyTypeModel>(_mapper);
+            return response;
+        }
+
+        public async Task<HobbyTypeModel> UpdateHobbyTypeDetailsAsync(long hobbyTypeId, HobbyTypeModel model)
+        {
+            var entity = _mapper.Map<HobbyTypeModel, HobbyType>(model);
+
+            await _unitOfWork.Repository<HobbyType>().UpdateAsync(entity);
+            await _unitOfWork.CompleteAsync();
+            return new HobbyTypeModel();
+        }
+
     }
 }
