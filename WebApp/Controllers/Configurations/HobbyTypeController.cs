c37e35fb2cd80a52a6cfa2c9811561545506972a
mdsobujislam <sobujislam674@gmail.com>
2022-12-28 16:07:31 +0600

    sobuj : add search n all

--- WebApp/Controllers/Configurations/HobbyTypeController.cs
+++ WebApp/Controllers/Configurations/HobbyTypeController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using WebApp.Core;
 using WebApp.Extensions;
 using WebApp.Helpers.Base;
+using WebApp.Service.Models.Configurations;
+using WebApp.Service.Models.Enrols;
 using WebApp.Service.Services.Configurations;
 using WebApp.Services;
 using WebApp.Sql.Entities.Configurations;
@@ -24,6 +27,43 @@ namespace WebApp.Controllers.Configurations
         public async Task<IActionResult> GetDropdownAsync(string searchText = null)
         {
             var res = await _hobbyTypeService.GetDropdownAsync(searchText);
+
+            return new ApiOkActionResult(res);
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> GetSearchAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string searchText = null)
+        {
+            var res = await _hobbyTypeService.GetSearchAsync(pageIndex, pageSize, searchText);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpGet("filter")]
+        public async Task<IActionResult> GetFilterAsync(int pageIndex = CommonVariables.pageIndex, int pageSize = CommonVariables.pageSize, string filterText1 = null /*string filterText2 = null*/)
+        {
+            var res = await _hobbyTypeService.GetFilterAsync(pageIndex, pageSize, filterText1 /*filterText2*/);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpGet("{hobbyTypeId}")]
+        public async Task<IActionResult> GetHobbyTypeDetailsAsync(long hobbyTypeId)
+        {
+            var res = await _hobbyTypeService.GetHobbyTypeDetailsAsync(hobbyTypeId);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpPost()]
+        public async Task<IActionResult> AddHobbyTypeDetailsAsync([FromForm] HobbyTypeModel model)
+        {
+            var res = await _hobbyTypeService.AddHobbyTypeDetailsAsync(model);
+
+            return new ApiOkActionResult(res);
+        }
+        [HttpPut("{hobbyTypeId}")]
+        public async Task<IActionResult> UpdateHobbyTypeDetailsAsync(long hobbyTypeId, [FromForm] HobbyTypeModel model)
+        {
+
+            var res = await _hobbyTypeService.UpdateHobbyTypeDetailsAsync(hobbyTypeId, model);
 
             return new ApiOkActionResult(res);
         }
