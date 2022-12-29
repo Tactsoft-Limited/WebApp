05b8f357a2997e2367d93de42f6da607c15e2f1b
mdsobujislam <sobujislam674@gmail.com>
2022-12-29 16:30:21 +0600

    sobuj : add documenttype property

--- WebApp.Sql/Entities/Enrols/Document.cs
+++ WebApp.Sql/Entities/Enrols/Document.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApp.Sql.Entities.Configurations;
 using static WebApp.Sql.Entities.Identities.IdentityModel;
 
 namespace WebApp.Sql.Entities.Enrols
@@ -16,5 +17,7 @@ namespace WebApp.Sql.Entities.Enrols
         public string Remark { get; set; }
         public User User { get; set; }
         public Employees Employees { get; set; }
+        public DocumentType DocumentType { get; set; }
+       
     }
 }
